5b525e3b3389c42061fa2dced0ff14bbc3dd08b5
Brennan <brennan8192@gmail.com>
2022-11-10 23:43:24 -0500

    REEEEEE

--- skeleton/interpreter/Interpreter.java
+++ skeleton/interpreter/Interpreter.java
@@ -109,30 +109,6 @@ public class Interpreter {
         }
     }
 
-    /* 
-    String executeRoot(Program astRoot, long arg) {
-        
-            //Long lastReturn = 0L;
-            //HashMap<String, Long> varMap = new HashMap<String, Long>();
-            //VarDecl argument = astRoot.getArgs();
-            //varMap.put(argument.getIdentifier(),arg);
-            //StatementList statements = astRoot.getStmtList();
-            //return String.valueOf(executeStmtList(statements, lastReturn, varMap));
-        Long lastReturn = 0L;
-        FuncDefList list = astRoot.getList();
-        populateFuncDef(list);
-        if(this.env.containsKey("main")){
-            FuncDef main = this.env.get("main");
-            HashMap<String,Long> argMap = new HashMap<String,Long>();
-            argMap.put(main.getFormalDeclList().getList().getFirst().getIdentifier(),arg);
-            lastReturn = executeStmtList(main.getStatementList(),lastReturn,argMap);
-        } else {
-            throw new RuntimeException("No Main function");
-        }
-
-        return String.valueOf(lastReturn);      
-    }
-*/
     Object executeRoot(Program astRoot, long arg) {
         FuncDefList list = astRoot.getList();
         populateFuncDef(list);
@@ -158,23 +134,7 @@ public class Interpreter {
             }
         }
     }
-/* 
-    Long executeStmtList(StatementList stmtList, Long ret, HashMap<String, Long> varMap){
-        StatementList currentStatementList = stmtList;
-        while(!currentStatementList.isEmpty()){
-            if(!this.hasReturned){
-                ret = executeStmt(currentStatementList.getStatement(), ret, varMap);
-            }
-            if(currentStatementList.hasNext()){
-                currentStatementList = currentStatementList.getNextStatement();
-            } else {
-                break;
-            }
-        }
 
-        return ret;
-    }
-    */
     Object executeStmtList(StatementList stmtList, HashMap<String,Object> varMap){
         Object retVal = executeStmt(stmtList.getStatement(),varMap);
         if(retVal != null){
@@ -228,49 +188,7 @@ public class Interpreter {
         }
     }
 
-    /* 
-    long executeStmt(Statement stmt, long lastReturn, HashMap<String, Long> varMap){
-        if(stmt instanceof VarDeclarationStatement){
-            VarDeclarationStatement temp = (VarDeclarationStatement)stmt;
-            VarDecl var = temp.getVarDecl();
-            Expr expr = temp.getExpr();
-            varMap.put(var.getIdentifier(),(Long) evaluate(expr, varMap));
-        } else if(stmt instanceof PrintStatement){
-            PrintStatement temp = (PrintStatement) stmt; 
-            Expr expr = temp.getExpr();
-            System.out.println(evaluate(expr, varMap));
-        } else if(stmt instanceof IfStatement){
-            IfStatement temp = (IfStatement) stmt;
-            Condition cond = temp.getCondition();
-            Statement ifStatement = temp.getStatement();
-            if(evalCondition(cond,varMap)){
-                lastReturn = executeStmt(ifStatement, lastReturn, varMap);
-            } 
-        } else if(stmt instanceof IfElseStatement){
-            IfElseStatement temp = (IfElseStatement) stmt;
-            Condition cond = temp.getCondition();
-            Statement ifStatement = temp.getIfStatement();
-            Statement elseStatement = temp.getElseStatement();
-            if(evalCondition(cond,varMap)){
-                lastReturn = executeStmt(ifStatement, lastReturn, varMap);
-            } else {
-                lastReturn = executeStmt(elseStatement, lastReturn, varMap);
-            }
-        } else if(stmt instanceof ReturnStatement){
-            ReturnStatement temp = (ReturnStatement) stmt;
-            Expr expr = temp.getExpr();
-            lastReturn = evaluate(expr, varMap);
-            this.hasReturned = true;
-        } else if(stmt instanceof StatementList){
-            StatementList temp = (StatementList) stmt;
-            lastReturn = executeStmtList(temp, lastReturn, varMap);
-        } else {
-            System.out.println("ERROR: SOMETHING WENT WWRONG");
-        }
-        return lastReturn;
-    }
-
-*/
+ 
 
     boolean evalCondition(Condition cond, HashMap<String, Object> varMap){
         if(cond instanceof NotOP){
@@ -318,6 +236,7 @@ public class Interpreter {
                 case BinaryExpr.PLUS: return (Long)evaluate(binaryExpr.getLeftExpr(), varMap) + (Long)evaluate(binaryExpr.getRightExpr(), varMap);
                 case BinaryExpr.MINUS: return (Long)evaluate(binaryExpr.getLeftExpr(), varMap) - (Long)evaluate(binaryExpr.getRightExpr(), varMap);
                 case BinaryExpr.TIMES: return (Long)evaluate(binaryExpr.getLeftExpr(), varMap) * (Long)evaluate(binaryExpr.getRightExpr(), varMap);
+                case BinaryExpr.DOT: return new QRef(new QObj((QVal) evaluate(binaryExpr.getLeftExpr(),varMap),(QVal) evaluate(binaryExpr.getRightExpr(),varMap)));
                 default: throw new RuntimeException("Unhandled operator");
             }
         } else if(expr instanceof UnaryExpr) {
@@ -364,71 +283,7 @@ public class Interpreter {
         }
     }
 
-    /*
-    long evaluate(Expr expr, HashMap<String, Long> varMap) {
-        if (expr instanceof ConstExpr) {
-            return ((ConstExpr)expr).getValue();
-        } else if (expr instanceof BinaryExpr) {
-            BinaryExpr binaryExpr = (BinaryExpr)expr;
-            switch (binaryExpr.getOperator()) {
-                case BinaryExpr.PLUS: return (Long)evaluate(binaryExpr.getLeftExpr(), varMap) + (Long)evaluate(binaryExpr.getRightExpr(), varMap);
-                case BinaryExpr.MINUS: return (Long)evaluate(binaryExpr.getLeftExpr(), varMap) - (Long)evaluate(binaryExpr.getRightExpr(), varMap);
-                case BinaryExpr.TIMES: return (Long)evaluate(binaryExpr.getLeftExpr(), varMap) * (Long)evaluate(binaryExpr.getRightExpr(), varMap);
-                default: throw new RuntimeException("Unhandled operator");
-            }
-        } else if(expr instanceof UnaryExpr) {
-            UnaryExpr unaryExpr = (UnaryExpr)expr;
-            return -1 * (Long)evaluate(unaryExpr.getExpr(), varMap);
-        } else if(expr instanceof IdentExpr){
-            IdentExpr temp = (IdentExpr) expr;
-            String varName = temp.getIdentifier();
-            return varMap.get(varName);
-        } else if(expr instanceof FuncExpr) {
-            FuncExpr temp = (FuncExpr)expr;
-            String ident = temp.getIdent();
-            ExprList list = temp.getExprList();
-            if(this.env.containsKey(ident)){
-                FuncDef func = this.env.get(ident);
-                HashMap<String,Long> vars = new HashMap<String,Long>();
-                NEmptyExprList exprList = null;
-                NEmptyFormalDeclList declList = null;
-                if(!list.isEmpty()){
-                    exprList = list.getList();
-                }
-                if(func.hasDeclList()){
-                    if(!func.getFormalDeclList().isEmpty()){
-                        declList = func.getFormalDeclList().getList();
-                    }
-                }
-                while(!exprList.isEmpty()){
-                    Expr temp2 = exprList.getFirst();
-                    VarDecl temp3 = declList.getFirst();
-                    vars.put(temp3.getIdentifier(),evaluate(temp2,varMap));
-                    if(exprList.hasNext()){
-                        exprList = exprList.getRest();
-                        declList = declList.getRest();
-                    } else {
-                        break;
-                    }
-                }
-                return executeStmtList(func.getStatementList(), 0L, vars);
-            } else if(ident.equals("randomInt")){
-                Random rand = new Random();
-                return rand.nextInt((int)evaluate(list.getList().getFirst(), varMap));
-            } else {
-                throw new RuntimeException("Invalid function call");
-            }
-        } else {
-            throw new RuntimeException("Unhandled Expr type");
-        }
-    }
-
-    */
-
-    boolean sameLength(NEmptyFormalDeclList declList, NEmptyExprList exprList){
-        return true;
-    }
-    
+  
 	public static void fatalError(String message, int processReturnCode) {
         System.out.println(message);
         System.exit(processReturnCode);
